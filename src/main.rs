e9d3f95c6c3f46d43ef89055279f640f0394077e
Jann Stute <jann.stute@protonmail.com>
2025-01-20 23:40:08 +0100

    add command arg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-fn main() {
+use std::env;
+
+fn print_usage() {
+    println!("Usage: {} <command>", env::args().nth(0).unwrap());
+    println!("Commands:");
+    println!("  help, -h, --help: Print this help message");
+    println!("  wallpaper: Set the current wallpaper");
+}
+
+fn set_wallpaper() {
     let result = utiltool::file_picker("/etc/nixos/common/wallpapers");
     if let Ok(paths) = result {
         if paths.len() > 0 {
@@ -10,5 +19,23 @@ fn main() {
         }
     } else {
         eprintln!("{}", result.unwrap_err());
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() <= 1 {
+        print_usage();
+        return;
+    }
+
+    match args[1].as_str() {
+        "help" | "-h" | "--help" => print_usage(),
+        "wallpaper" => set_wallpaper(),
+        _ => {
+            eprintln!("Unknown command: {}", args[1]);
+            print_usage();
+        }
     }
 }
