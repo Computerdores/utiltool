9f354d888fabfe465ed768431fb71660160c1fa3
Jann Stute <jann.stute@protonmail.com>
2025-06-19 14:54:00 +0200

    feat: store config options as yaml

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -5,11 +5,25 @@ let
 in {
     options.programs.utiltool = {
         enable = lib.mkEnableOption "Enable utiltool.";
+        pick_file_script = lib.mkOption {
+            type = lib.types.str;
+            default = ''
+                t=$(mktemp -t 'utiltool-pick-file.XXXXXX')
+                yazi --chooser-file=$t
+                cat $t
+                rm $t
+            '';
+            description = "A bash script to pick one or more files. Should output one file path per line to stdout.";
+        };
     };
 
     config = lib.mkIf cfg.enable {
         home.packages = [
             utiltoolPkg
         ];
+
+        xdg.configFile."utiltool/config.yaml".text = lib.generators.toYAML {} {
+            pick_file_script = cfg.pick_file_script;
+        };
     };
 }
