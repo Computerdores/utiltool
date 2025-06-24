8948438724a0f8de0adebeb54c30295ff4cf7574
Jann Stute <jann.stute@protonmail.com>
2025-06-24 10:43:03 +0200

    fix: provide default values for nix options (v0.5.1)

--- nix/hm-module.nix
+++ nix/hm-module.nix
@@ -7,6 +7,7 @@ in {
         enable = lib.mkEnableOption "Enable utiltool.";
         pick_file_script = lib.mkOption {
             type = lib.types.str;
+            default = "";
             example = ''
                 t=$(mktemp -t 'utiltool-pick-file.XXXXXX')
                 yazi --chooser-file=$t
@@ -19,36 +20,42 @@ in {
         system = {
             shutdown_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "shutdown now";
                 description = "A bash script to shutdown the system.";
             };
 
             reboot_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "reboot";
                 description = "A bash script to reboot the system.";
             };
 
             hibernate_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "systemctl hibernate";
                 description = "A bash script to hibernate the system. Before running this script, the lock script will be run as well.";
             };
 
             suspend_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "systemctl suspend";
                 description = "A bash script to suspend the system. Before running this script, the lock script will be run as well.";
             };
 
             logout_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "uwsm stop";
                 description = "A bash script to logout of the current wayland session.";
             };
 
             lock_script = lib.mkOption {
                 type = lib.types.str;
+                default = "";
                 example = "hyprctl --immediate >/dev/null";
                 description = "A bash script to lock the current wayland session.";
             };
